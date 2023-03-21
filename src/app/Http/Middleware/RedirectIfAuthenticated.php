ff63402076453b7081e9f3ef4202973fdda4bfef
ptr-ito <ptr2839@gmail.com>
2023-03-21 14:11:41 +0900

    [add] 認証情報を追記 #3

--- src/app/Http/Middleware/RedirectIfAuthenticated.php
+++ src/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,10 +21,11 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return response()->json([
+                    'message' => '既に認証済みです'
+                ], 200);
             }
         }
-
         return $next($request);
     }
 }
